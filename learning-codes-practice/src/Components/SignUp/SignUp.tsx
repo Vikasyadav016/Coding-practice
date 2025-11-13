de1ffaaec595eb268b08ee4026d517e4123646b1
Vikasyadav016 <77086537+Vikasyadav016@users.noreply.github.com>
2025-11-13 11:55:59 +0000

    Added custom hook for sign up form page and habdle all functioning by custom hook.

--- learning-codes-practice/src/Components/SignUp/SignUp.tsx
+++ learning-codes-practice/src/Components/SignUp/SignUp.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Modal, Button, Accordion, Form, Row, Col } from "react-bootstrap";
 import State from "../../MasterData/State.json"
 import District from "../../MasterData/District.json"
+import useSignUpForm from "./signUpFormValidations";
 
 interface SignupModalProps {
     show: boolean;
@@ -10,94 +11,17 @@ interface SignupModalProps {
 }
 
 const SignupModal: React.FC<SignupModalProps> = ({ show, handleClose, handleShow }) => {
-    const [activeStep, setActiveStep] = useState<string | null>("0");
-    const [errors, setErrors] = useState<any>({});
-    const [formData, setFormData] = useState<any>({
-        name: "",
-        email: "",
-        aadhar: "",
-        mobile: "",
-        fatherName: "",
-        motherName: "",
-        guardianMobile: "",
-        guardianEmail: "",
-        state: "",
-        district: "",
-        block: "",
-        panchayat: "",
-        village: "",
-        ward: "",
-        houseNo: "",
-        landmark: "",
-        area: "",
-        pincode: "",
-        password: "",
-        confirmPassword: "",
-        termsAccepted: false
-    });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement> | any) => {
-        const { name, value } = e.target;
-        setFormData((prev: any) => ({ ...prev, [name]: value }));
-        setErrors((prev: any) => ({ ...prev, [name]: "" })); // clear error on change
-    };
-
-    const validateStep1 = () => {
-        const newErrors: any = {};
-        if (!formData.name) newErrors.name = "Name is required";
-        if (!formData.email) newErrors.email = "Email is required";
-        if (!formData.aadhar) newErrors.aadhar = "Aadhar number is required";
-        if (!formData.mobile) newErrors.mobile = "Mobile number is required";
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    const validateStep2 = () => {
-        const newErrors: any = {};
-        if (!formData.fatherName) newErrors.fatherName = "Father's name is required";
-        if (!formData.motherName) newErrors.motherName = "Mother's name is required";
-        if (!formData.guardianMobile) newErrors.guardianMobile = "Guardian mobile is required";
-        if (!formData.guardianEmail) newErrors.guardianEmail = "Guardian email is required";
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    const validateStep3 = () => {
-        const newErrors: any = {};
-        if (!formData.state) newErrors.state = "State is required";
-        if (!formData.district) newErrors.district = "District is required";
-        if (!formData.block) newErrors.block = "Block is required";
-        if (!formData.panchayat) newErrors.panchayat = "panchayat is required";
-        if (!formData.village) newErrors.village = "Village is required";
-        if (!formData.pincode) newErrors.pincode = "Pincode is required";
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-    const validateStep4 = () => {
-        const newErrors: any = {};
-        if (!formData.password) newErrors.password = "Password is required";
-        if (formData.password !== formData.confirmPassword)
-            newErrors.confirmPassword = "Passwords do not match";
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    const handleNext = (nextStep: string) => {
-        let valid = false;
-        if (activeStep === "0") valid = validateStep1();
-        if (activeStep === "1") valid = validateStep2();
-        if (activeStep === "2") valid = validateStep3();
-        if (activeStep === "3") valid = validateStep4();
-
-        if (valid) setActiveStep(nextStep);
-    };
-
-    const handlePrev = (step: string) => setActiveStep(step);
-
-
-    const handleFormFinalSubmit = () => {
-        alert(JSON.stringify)
-    }
+    const {
+        activeStep,
+        errors,
+        formData,
+        setFormData,
+        handleChange,
+        handleNext,
+        handlePrev,
+        handleFormFinalSubmit,
+    } = useSignUpForm()
 
 
     return (
