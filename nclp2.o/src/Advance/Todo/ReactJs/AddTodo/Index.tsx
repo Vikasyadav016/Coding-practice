976499f6858df33cfdc82edcf0960eebe92277e0
Vikasyadav016 <77086537+Vikasyadav016@users.noreply.github.com>
2026-05-26 10:27:26 +0000

    Add to do

--- nclp2.o/src/Advance/Todo/ReactJs/AddTodo/Index.tsx
+++ nclp2.o/src/Advance/Todo/ReactJs/AddTodo/Index.tsx
@@ -1,10 +1,109 @@
+import { useState } from "react";
+import "./index.css";
+import ViewTodoDetails from "../ViewTodo";
+
+type BookDetails = {
+    bookTitle: string;
+    bookPrice: string;
+    bookDescription: string;
+};
+
 const AddUpdateTodo = () => {
+    const [bookDetails, setBookDetails] = useState<BookDetails>({
+        bookTitle: "",
+        bookPrice: "",
+        bookDescription: ""
+    });
+    const [bookList, setBookList] = useState<BookDetails[]>([]);
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { name, value } = e.target;
+
+        setBookDetails((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleAddBook = () => {
+        if (
+            !bookDetails.bookTitle ||
+            !bookDetails.bookPrice ||
+            !bookDetails.bookDescription
+        ) {
+            alert("Please fill all fields");
+            return;
+        }
+        setBookList((prev) => [...prev, bookDetails]);
+        setBookDetails({
+            bookTitle: "",
+            bookPrice: "",
+            bookDescription: ""
+        });
+    };
 
     return (
-        <div>
-            <h1>Add/Update To do</h1>
+        <div className="maincontainer">
+            <h1>Add/Update Your Book</h1>
+
+            <div className="addformsection">
+
+                {/* Book Title */}
+                <div className="formgroup">
+                    <label className="label">Book Title</label>
+
+                    <div className="inputfield">
+                        <input
+                            className="input"
+                            type="text"
+                            name="bookTitle"
+                            value={bookDetails.bookTitle}
+                            onChange={handleInputChange}
+                            placeholder="Enter book title"
+                        />
+                    </div>
+                </div>
+
+                {/* Book Price */}
+                <div className="formgroup">
+                    <label className="label">Book Price</label>
+
+                    <div className="inputfield">
+                        <input
+                            className="input"
+                            type="text"
+                            name="bookPrice"
+                            value={bookDetails.bookPrice}
+                            onChange={handleInputChange}
+                            placeholder="Enter book price"
+                        />
+                    </div>
+                </div>
+
+                {/* Book Description */}
+                <div className="formgroup">
+                    <label className="label">Book Description</label>
+
+                    <div className="inputfield">
+                        <textarea
+                            className="textarea"
+                            name="bookDescription"
+                            value={bookDetails.bookDescription}
+                            onChange={handleInputChange}
+                            placeholder="Enter description"
+                        />
+                    </div>
+                </div>
+
+                <button className="submitbtn" onClick={handleAddBook}>
+                    Save Book
+                </button>
+            </div>
+            {bookList.length > 0 && <ViewTodoDetails bookList={bookList} />}
         </div>
-    )
-} 
+    );
+};
 
 export default AddUpdateTodo;
--- nclp2.o/src/Advance/Todo/ReactJs/ViewTodo/index.tsx
+++ nclp2.o/src/Advance/Todo/ReactJs/ViewTodo/index.tsx
@@ -1,10 +1,43 @@
-const ViewTodoDetails = () => {
+import "./index.css";
 
-    return (
-        <div>
-            <h1>View To do Details</h1>
-        </div>
-    )
+interface BookType {
+    bookTitle: string;
+    bookPrice: string;
+    bookDescription: string;
 }
 
-export default ViewTodoDetails
+interface ViewTodoDetailsProps {
+    bookList: BookType[];
+}
+
+const ViewTodoDetails = ({ bookList }: ViewTodoDetailsProps) => {
+    return (
+        <div className="maincontainer">
+            {/* BOOK CARD LIST */}
+            <h1>View Your Book List </h1>
+            <div className="booklistcontainer">
+                {bookList && bookList.length > 0 ? bookList.map((book, index) => (
+                    <div className="bookcard" key={index}>
+                        <h2 className="booktitle">
+                            {book.bookTitle}
+                        </h2>
+                        <p className="bookprice">
+                            ₹ {book.bookPrice}
+                        </p>
+                        <p className="bookdescription">
+                            {book.bookDescription}
+                        </p>
+                    </div>
+                )) :
+                    <div className="nodatafound">
+                        <h4>No data found</h4>
+                    </div>
+                }
+
+            </div>
+
+        </div>
+    );
+};
+
+export default ViewTodoDetails;
